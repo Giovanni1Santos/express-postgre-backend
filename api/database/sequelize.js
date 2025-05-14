a85599e63b144152cee7d3ededdd9d01554a2bff
Giovanni Santos <166954268+Giovanni1Santos@users.noreply.github.com>
2025-05-14 13:20:30 +0000

    s

--- api/database/sequelize.js
+++ api/database/sequelize.js
@@ -1,12 +1,23 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-// Montando manualmente a connection string a partir das variáveis
 const sequelize = new Sequelize(
   `postgres://${process.env.POSTGRES_USER}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
   {
     dialect: 'postgres',
     logging: false,
+    dialectOptions: {
+      ssl: process.env.NODE_ENV === 'production' ? {
+        require: true,
+        rejectUnauthorized: false
+      } : false
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000
+    }
   }
 );
 
