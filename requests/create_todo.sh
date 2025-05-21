7516140b1629c210de6ea37caedb08b62c977606
Giovanni Santos <166954268+Giovanni1Santos@users.noreply.github.com>
2025-05-21 01:45:25 +0000

    .

--- requests/create_todo.sh
+++ requests/create_todo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTc0NzU5OTUxMywiZXhwIjoxNzQ3NjAzMTEzfQ.YlJ4jLvHsK81rAx_MrxuTMjMjp-_yqtPn1sN4piQvK0"
+TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTc0Nzc5MTUyNCwiZXhwIjoxNzQ3Nzk1MTI0fQ.eNx1uZqe2eG37ZuJA9-xBH_Ws6"
 
 curl -X POST http://localhost:3000/api/todos \
   -H "Authorization: Bearer $TOKEN" \
